5988af3e35477c878fd3271218456c416eefb2ae
codewithshraddha25 <shraddhasjagdale@gmail.com>
2024-09-05 22:49:56 +0530

    change in code for majority element

--- src/Arrays/MajorityElement.java
+++ src/Arrays/MajorityElement.java
@@ -1,17 +1,18 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class MajorityElement {
 
 	public static int majorityElement(int[] nums) {
-        int maxEle=nums[0];
-        for(int i=1;i<nums.length;i++){
-            maxEle=Math.max(nums[i],maxEle);
-        }
-        return maxEle;
+		Arrays.sort(nums);//n logn time
+        int index= (nums.length/2);//once we sort array , 100% chance is that repeated element occurs in middle element after sort
+        return nums[index];
+
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int nums[]= {2,2,1,1,3,4,5};
+		int nums[]= {2,2,2,1,1,3,3,3,3,4,5};
 		System.out.println(majorityElement(nums));
 
 	}
