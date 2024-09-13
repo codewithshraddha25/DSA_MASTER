94b67b0e9fffe4dcaa48b6aad56c71f07d237c3f
codewithshraddha25 <shraddhasjagdale@gmail.com>
2024-09-14 00:13:27 +0530

    move zeroes array problem

---
+++ src/Arrays/moveZeroes.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class moveZeroes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		moveZeroes( new int[] {1,0,2,13,0,0,4});
+	}
+	
+	public static void moveZeroes(int[] nums) {
+        int insertPosition=0;
+        for(int i=0;i<nums.length;i++){
+            //fill all non zero numbers
+            if(nums[i] != 0){
+                nums[insertPosition]=nums[i];
+                insertPosition++;
+            }
+        }
+        while(insertPosition < nums.length){//to add zeros after non zeros
+            nums[insertPosition++]=0;
+        }
+       for(int i=0;i<nums.length;i++) {
+    	   System.out.println(nums[i]);
+       }
+    }
+
+}
