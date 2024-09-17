c66e18f8f230734702799af4ea25b286342be9bb
codewithshraddha25 <shraddhasjagdale@gmail.com>
2024-09-17 20:53:34 +0530

    corrected indentations

--- src/Arrays/SearchRotatedSortedArr.java
+++ src/Arrays/SearchRotatedSortedArr.java
@@ -10,27 +10,27 @@ public class SearchRotatedSortedArr {
 
 	}
 	public static int search(int[] nums, int target) {
-        return modifiedBinarySearch(nums,target,0, nums.length-1);
-    }
-    private static int modifiedBinarySearch(int[] arr, int target, int left, int right){
-        if(left>right)
-        return -1;
+		return modifiedBinarySearch(nums,target,0, nums.length-1);
+	}
+	private static int modifiedBinarySearch(int[] arr, int target, int left, int right){
+		if(left>right)
+			return -1;
 
-        int mid= left + ((right-left)/2);
-        if(arr[mid]==target)
-        return mid;
+		int mid= left + ((right-left)/2);
+		if(arr[mid]==target)
+			return mid;
 
-        if(arr[mid] >= arr[left]){
-            if(arr[left] <= target && target <= arr[mid])
-            return modifiedBinarySearch(arr,target, left, mid-1);
-            else
-            return modifiedBinarySearch(arr,target,mid+1,right);
-        }else{
-            if(arr[mid] <= target && target <= arr[right])
-            return modifiedBinarySearch(arr, target, mid+1, right);
-            else
-            return modifiedBinarySearch(arr,target, left, mid-1);
-        }
-    }
+		if(arr[mid] >= arr[left]){
+			if(arr[left] <= target && target <= arr[mid])
+				return modifiedBinarySearch(arr,target, left, mid-1);
+			else
+				return modifiedBinarySearch(arr,target,mid+1,right);
+		}else{
+			if(arr[mid] <= target && target <= arr[right])
+				return modifiedBinarySearch(arr, target, mid+1, right);
+			else
+				return modifiedBinarySearch(arr,target, left, mid-1);
+		}
+	}
 
 }
